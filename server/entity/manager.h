527bff75fdd9b51425580cfd30358a3a6bc93692
igor725 <gvaldovigor@gmail.com>
2024-08-15 03:07:19 +0300

    Minor fixes

--- server/entity/manager.h
+++ server/entity/manager.h
@@ -6,6 +6,7 @@
 
 #include <functional>
 #include <memory>
+#include <thread>
 
 class IEntityManager {
   public:
--- server/platform/unix.cpp
+++ server/platform/unix.cpp
@@ -1,5 +1,21 @@
+#define _GNU_SOURCE
 #include "platform.h"
 
+#include <pthread.h>
+#include <signal.h>
+
+namespace {
+void (*g_ctrlHandler)(void) = nullptr;
+}
+
 namespace Platform {
-void SetCurrentThreadName(std::string_view name) {}
+void SetCurrentThreadName(std::string_view name) {
+  pthread_setname_np(pthread_self(), name.data());
+}
+
+void RegisterCtrlCHandler(void (*func)(void)) {
+  g_ctrlHandler = func;
+
+  signal(SIGINT, [](int) -> void { g_ctrlHandler(); });
+}
 }; // namespace Platform
