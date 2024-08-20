7413687fc89f142d99e51b9f9828d19f2c92e61e
igor725 <gvaldovigor@gmail.com>
2024-08-20 22:51:51 +0300

    Minor fixes

--- server/mcregion/regionfile.cpp
+++ server/mcregion/regionfile.cpp
@@ -2,15 +2,12 @@
 
 #include <array>
 #include <chrono>
+#include <filesystem>
 #include <fstream>
 #include <vector>
 
 namespace {
 constexpr uint32_t SECTOR_SIZE = 4096;
-
-static inline uint32_t regOffset(const IntVector2& cpos) {
-  return 4 * (cpos.x & 31) + (cpos.z & 31) * 32;
-}
 } // namespace
 
 class InvalidRegionFileException;
@@ -20,14 +17,18 @@ class RegionFile: public IRegionFile {
 
   public:
   RegionFile(const std::string& fname): m_lastAccess(), m_offsets({0}) {
-    m_file.exceptions(m_file.exceptions() | std::ios::failbit | std::fstream::badbit);
-    m_file.open(fname, std::ios::binary | std::ios::in | std::ios::out | std::ios::app);
+    if (!std::filesystem::exists(fname)) {
+      std::ofstream(fname).close();
+    }
 
-    m_file.seekg(0, std::ios::end);
+    m_file.exceptions(m_file.exceptions() | std::ios::failbit | std::fstream::badbit);
+    m_file.open(fname, std::ios::in | std::ios::out | std::ios::binary | std::ios::ate);
+
     size_t fsize = m_file.tellg();
     m_file.seekg(0, std::ios::beg);
 
     if (fsize < SECTOR_SIZE) {
+      m_file.seekp(0, std::ios::beg);
       uint32_t empty = 0;
       for (int i = 0; i < 1024; ++i) {
         m_file.write(reinterpret_cast<char*>(&empty), 4);
@@ -35,12 +36,14 @@ class RegionFile: public IRegionFile {
       fsize = SECTOR_SIZE;
     }
 
-    m_file.seekg(0, std::ios::beg);
-
     {
+      m_file.seekp(0, std::ios::end);
       char empty = '\0';
-      for (; (fsize & 4095) != 0; ++fsize)
+      for (; (fsize & 4095) != 0; ++fsize) {
         m_file.write(&empty, 1);
+      }
+
+      m_file.flush();
     }
 
     m_file.seekg(0, std::ios::beg);
@@ -53,10 +56,13 @@ class RegionFile: public IRegionFile {
     uint32_t sec_count = 1;
     for (auto it = m_offsets.begin(); it != m_offsets.end(); ++it) {
       auto offset = (*it);
-      if (offset > 0) sec_count = std::max(sec_count, (offset >> 8) + (offset & 255) + 1);
+
+      auto secNum     = offset >> 8;
+      auto secAlloced = offset & 255;
+
+      if (offset > 0 && (secNum + secAlloced) <= m_sectorFree.size()) changeFree(secNum, secAlloced, false);
     }
 
-    if (sec_count > m_sectorFree.size()) m_sectorFree.resize(sec_count, false);
     m_lastAccess = std::chrono::system_clock::now();
   }
 
@@ -80,15 +86,13 @@ class RegionFile: public IRegionFile {
     if (sectorNum != 0 && sectorsAlloc == sectorsNeed) {
       return writeToSector(sectorNum, chunk);
     } else {
-      for (int i = 0; i < sectorsAlloc; ++i) {
-        m_sectorFree.at(sectorNum + i) = true;
-      }
-
-      auto currSect = std::find(m_sectorFree.begin(), m_sectorFree.end(), true);
+      // Free now unused sectors
+      changeFree(sectorNum, sectorsAlloc, true);
 
       uint32_t freeSectorStart = 0;
       uint32_t freeSectorsNum  = 0;
 
+      auto currSect = std::find(m_sectorFree.begin(), m_sectorFree.end(), true);
       for (; currSect != m_sectorFree.end(); ++currSect) {
         if (freeSectorsNum > 0) {
           freeSectorsNum = *currSect ? ++freeSectorsNum : 0;
@@ -102,23 +106,21 @@ class RegionFile: public IRegionFile {
 
       if (freeSectorsNum >= sectorsNeed) {
         sectorNum = freeSectorStart;
-        for (int i = 0; i < sectorsNeed; ++i) {
-          m_sectorFree.at(freeSectorStart + i) = false;
-        }
-        writeToSector(sectorNum, chunk);
+        changeFree(freeSectorStart, sectorsNeed, false);
       } else {
-        m_file.seekp(0, std::ios::end);
+        m_file.flush();
         sectorNum = m_sectorFree.size();
+        m_sectorFree.resize(sectorNum + sectorsNeed);
 
+        m_file.seekp(0, std::ios::end);
         for (int i = 0; i < sectorsNeed; ++i) {
           m_file.write(m_emptySector.data(), m_emptySector.size());
-          m_sectorFree.push_back(false);
         }
-
-        writeToSector(sectorNum, chunk);
       }
 
+      if (!writeToSector(sectorNum, chunk)) return false;
       setOffset(pos, (sectorNum << 8) | sectorsNeed);
+      return true;
     }
 
     return false;
@@ -132,12 +134,18 @@ class RegionFile: public IRegionFile {
   }
 
   private:
+  void changeFree(uint32_t begin, uint32_t length, bool value) {
+    auto it = m_sectorFree.begin() + begin;
+    std::fill(it, it + length, value);
+  }
+
   bool writeToSector(uint32_t snum, Chunk& chunk) {
     m_file.seekp(snum * SECTOR_SIZE, std::ios::beg);
     m_file.write(reinterpret_cast<const char*>(chunk.m_blocks.data()), chunk.m_blocks.size());
     m_file.write(reinterpret_cast<const char*>(chunk.m_meta.data()), chunk.m_meta.size());
     m_file.write(reinterpret_cast<const char*>(chunk.m_light.data()), chunk.m_light.size());
     m_file.write(reinterpret_cast<const char*>(chunk.m_sky.data()), chunk.m_sky.size());
+    m_file.flush();
     return true;
   }
 
@@ -156,8 +164,9 @@ class RegionFile: public IRegionFile {
     auto idx = getIndex(pos);
 
     m_offsets[idx] = offset;
-    m_file.seekp(idx * 4);
+    m_file.seekp(idx * 4, std::ios::beg);
     m_file.write((char*)&(m_offsets[idx]), 4);
+    m_file.flush();
   }
 
   std::chrono::system_clock::time_point m_lastAccess;
