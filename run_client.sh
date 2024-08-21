abd229742b5b704bb140cef0150238a7a23a39f0
igor725 <gvaldovigor@gmail.com>
2024-08-21 17:37:54 +0300

    Whoops

--- run_client.sh
+++ run_client.sh
@@ -4,4 +4,4 @@ if [ "$1" == "untouched" ]; then
   java -Djava.library.path="./client/natives/" -cp "./client/jars/*" net.minecraft.client.Minecraft "Test$RANDOM" "-"
 else
   java -Djava.library.path="./client/natives/" -cp "./client/jars/*" net.minecraft.launchwrapper.Launch "Test%random%" "-" --tweakClass net.minecraft.launchwrapper.VanillaTweaker --gameDir "./client/data/" --version "b1.7.3" --userType "legacy" --server "127.0.0.1" --port 25565
-end
+fi
